737f9c66d9f74f54fb43155b0fd6a966b2a5668b
Jarosław <jarek.kow@gmail.com>
2018-06-11 18:29:42 +0200

    add function hide unused column

--- src/filereader/MainApp.java
+++ src/filereader/MainApp.java
@@ -12,6 +12,7 @@ import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import org.apache.poi.hssf.usermodel.HSSFFormulaEvaluator;
+import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
@@ -173,7 +174,6 @@ public class MainApp extends javax.swing.JFrame {
                 Cell cellDlugosc = null;
                 Cell cellSztuki = null;
                 
-
                 for (int i = 0; i < blockList.size(); i++) {
                     System.out.println(blockList.get(i));
 
@@ -186,68 +186,79 @@ public class MainApp extends javax.swing.JFrame {
                     cellSrednica.setCellValue(blockList.get(i).getSrednica());
                     cellDlugosc.setCellValue(blockList.get(i).getDlugosc());
                     cellSztuki.setCellValue(blockList.get(i).getSztuki());
-                    
-                  
-                    
-//                    if(blockList.get(i).getSrednica() != 6) {
-//                        worksheet.setColumnHidden(4, true);
-//                    } 
-//                    
-//                    if (blockList.get(i).getSrednica() != 8) {
-//                        worksheet.setColumnHidden(5, true);
-//                    }
-//                    
-//                    if (blockList.get(i).getSrednica() != 10) {
-//                        worksheet.setColumnHidden(6, true);
-//                    }
-//                    
-//                    if (blockList.get(i).getSrednica() != 12) {
-//                        worksheet.setColumnHidden(7, true);
-//                    }
-//                    
-//                    if (blockList.get(i).getSrednica() != 14) {
-//                        worksheet.setColumnHidden(8, true);
-//                    }
-//                    
-//                    if (blockList.get(i).getSrednica() != 16) {
-//                        worksheet.setColumnHidden(9, true);
-//                    }
-//                    
-//                    if (blockList.get(i).getSrednica() != 18) {
-//                        worksheet.setColumnHidden(10, true);
-//                    }
-//                    
-//                    if (blockList.get(i).getSrednica() != 20) {
-//                        worksheet.setColumnHidden(11, true);
-//                    }
-//                    
-//                    if (blockList.get(i).getSrednica() != 22) {
-//                        worksheet.setColumnHidden(12, true);
-//                    }
-//                    
-//                    if (blockList.get(i).getSrednica() != 25) {
-//                        worksheet.setColumnHidden(13, true);
-//                    }
-//                    
-//                    if (blockList.get(i).getSrednica() != 28) {
-//                        worksheet.setColumnHidden(14, true);
-//                    }
-//                    
-//                    if (blockList.get(i).getSrednica() != 32) {
-//                        worksheet.setColumnHidden(15, true);
-//                    }                     
-                    
-                    
                 }
                 
                 int startHideRow = blockList.size() + 3;
-                int endHideRow = 202;   // Number depends on Excel sheet. Remember (lastRowToDelete - 1)
-               
+                int endHideRow = 202;
                 for (int i = startHideRow; i <= endHideRow; i++) {
                     worksheet.getRow(i).setHeight((short) 0);
                 }
                 
+                // Refreshing FormulaCells
                 HSSFFormulaEvaluator.evaluateAllFormulaCells(wb);
+                
+                Cell fi6 = worksheet.getRow(endHideRow + 1).getCell(4);
+                Cell fi8 = worksheet.getRow(endHideRow + 1).getCell(5);
+                Cell fi10 = worksheet.getRow(endHideRow + 1).getCell(6);
+                Cell fi12 = worksheet.getRow(endHideRow + 1).getCell(7);
+                Cell fi14 = worksheet.getRow(endHideRow + 1).getCell(8);
+                Cell fi16 = worksheet.getRow(endHideRow + 1).getCell(9);
+                Cell fi18 = worksheet.getRow(endHideRow + 1).getCell(10);
+                Cell fi20 = worksheet.getRow(endHideRow + 1).getCell(11);
+                Cell fi22 = worksheet.getRow(endHideRow + 1).getCell(12);
+                Cell fi25 = worksheet.getRow(endHideRow + 1).getCell(13);
+                Cell fi28 = worksheet.getRow(endHideRow + 1).getCell(14);
+                Cell fi32 = worksheet.getRow(endHideRow + 1).getCell(15);
+                
+                if (fi6.getNumericCellValue() == 0) {
+                    worksheet.setColumnHidden(4,true);
+                }
+                
+                if (fi8.getNumericCellValue() == 0) {
+                    worksheet.setColumnHidden(5,true);
+                }
+
+                if (fi10.getNumericCellValue() == 0) {
+                    worksheet.setColumnHidden(6,true);
+                }
+
+                if (fi12.getNumericCellValue() == 0) {
+                    worksheet.setColumnHidden(7,true);
+                }
+
+                if (fi14.getNumericCellValue() == 0) {
+                    worksheet.setColumnHidden(8,true);
+                }
+
+                if (fi16.getNumericCellValue() == 0) {
+                    worksheet.setColumnHidden(9,true);
+                }
+
+                if (fi18.getNumericCellValue() == 0) {
+                    worksheet.setColumnHidden(10,true);
+                }
+
+                if (fi20.getNumericCellValue() == 0) {
+                    worksheet.setColumnHidden(11,true);
+                }
+
+                if (fi22.getNumericCellValue() == 0) {
+                    worksheet.setColumnHidden(12,true);
+                }
+
+                if (fi25.getNumericCellValue() == 0) {
+                    worksheet.setColumnHidden(13,true);
+                }
+
+                if (fi28.getNumericCellValue() == 0) {
+                    worksheet.setColumnHidden(14,true);
+                }
+
+                if (fi32.getNumericCellValue() == 0) {
+                    worksheet.setColumnHidden(15,true);
+                }                
+                
+                
                 wb.createSheet("sheet");
                 fileInputStream.close();
 
